60ec008a9662ee5036db12af9092b48f3097b927
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-07-11 14:56:29 +0300

    refactor: rename main class

--- src/main/java/ru/skillbox/contacts/App.java
+++ src/main/java/ru/skillbox/contacts/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactsApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactsApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }

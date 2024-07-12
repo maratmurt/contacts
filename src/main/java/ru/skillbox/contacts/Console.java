acd2efc8f00a916563768d2a2667a97091c7ade4
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-07-12 18:16:11 +0300

    refactor: extract Console

--- src/main/java/ru/skillbox/contacts/Console.java
+++ src/main/java/ru/skillbox/contacts/Console.java
@@ -12,13 +12,15 @@ import java.util.Scanner;
 @RequiredArgsConstructor
 public class Console implements ApplicationRunner {
     private final ContactsService contactsService;
+    private boolean running;
+    private Scanner scanner;
 
     @Override
     public void run(ApplicationArguments args) {
-        boolean running = true;
+        running = true;
 
         while (running) {
-            Scanner scanner = new Scanner(System.in);
+            scanner = new Scanner(System.in);
             System.out.println("\nВыберите действие:\n" +
                     "1 - Просмотреть все контакты\n" +
                     "2 - Добавить контакт\n" +
@@ -28,45 +30,49 @@ public class Console implements ApplicationRunner {
             );
             String command = scanner.nextLine();
 
-            switch (command) {
-                case "1" -> {
-                    contactsService.print();
+            handleInput(command);
+        }
+    }
+
+    private void handleInput(String command) {
+        switch (command) {
+            case "1" -> {
+                contactsService.print();
+            }
+            case "2" -> {
+                System.out.println("Введите данные в следующем формате:\n" +
+                        "Фамилия Имя Отчество;+890999999;user@domain.name");
+                String contactData = scanner.nextLine();
+                try {
+                    contactsService.add(contactData);
+                    System.out.println("Контакт добавлен");
+                } catch (AddContactException e) {
+                    System.err.println(e.getMessage());
                 }
-                case "2" -> {
-                    System.out.println("Введите данные в следующем формате:\n" +
-                            "Фамилия Имя Отчество;+890999999;user@domain.name");
-                    String contactData = scanner.nextLine();
-                    try {
-                        contactsService.add(contactData);
-                        System.out.println("Контакт добавлен");
-                    } catch (AddContactException e) {
-                        System.err.println(e.getMessage());
-                    }
+            }
+            case "3" -> {
+                System.out.println("Введите email");
+                String email = scanner.nextLine();
+                if (contactsService.delete(email)) {
+                    System.out.println("Контакт удалён");
+                } else {
+                    System.err.println("Контакт не найден!");
                 }
-                case "3" -> {
-                    System.out.println("Введите email");
-                    String email = scanner.nextLine();
-                    if (contactsService.delete(email)) {
-                        System.out.println("Контакт удалён");
-                    } else {
-                        System.err.println("Контакт не найден!");
-                    }
+            }
+            case "4" -> {
+                try {
+                    contactsService.write();
+                    System.out.println("Данные сохранены");
+                } catch (FileNotFoundException e) {
+                    System.err.println("Файл не найден");
                 }
-                case "4" -> {
-                    try {
-                        contactsService.write();
-                        System.out.println("Данные сохранены");
-                    } catch (FileNotFoundException e) {
-                        System.err.println("Файл не найден");
-                    }
-                }
-                case "0" -> {
-                    running = false;
-                    System.out.println("До встречи!");
-                }
-                default -> {
-                    System.err.println("Не корректный ввод!\n");
-                }
+            }
+            case "0" -> {
+                running = false;
+                System.out.println("До встречи!");
+            }
+            default -> {
+                System.err.println("Не корректный ввод!\n");
             }
         }
     }
